daa2f41d81f52b1761c59c5c7096dc5e32ab5396
manishchub <tabnexus12@gmail.com>
2022-08-13 15:19:51 +0530

    Add files via upload

---
+++ DLSimServer.ino
@@ -0,0 +1,203 @@
+#include <WiFi.h>                                     // needed to connect to WiFi
+#include <WebServer.h>                                // needed to create a simple webserver
+#include <WebSocketsServer.h>                         // needed for instant communication between client and server through Websockets
+#include <ArduinoJson.h>          // needed for JSON encapsulation (send multiple variables with one string)
+#include <WebSocketsClient.h>
+#include <FS.h>
+#include "SPIFFS.h"
+
+
+#define FORMAT_SPIFFS_IF_FAILED true
+
+
+// SSID and password of Wifi connection:
+const char* ssid = "netter";
+const char* password = "1212121212";
+
+// The String below "webpage" contains the complete HTML code that is sent to the client whenever someone connects to the webserver
+String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Lets generate a random number</h1><p>The first random number is: <span id='rand1'>-</span></p><p>The second random number is: <span id='rand2'>-</span></p><p><button type='button' id='BTN_SEND_BACK'>Send info to ESP32</button></p></span></body><script> var Socket; document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back); function init() { Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) { processCommand(event); }; } function button_send_back() { var msg = {brand: 'Gibson',type: 'Les Paul Studio',year: 2010,color: 'white'};Socket.send(JSON.stringify(msg)); } function processCommand(event) {var obj = JSON.parse(event.data);document.getElementById('rand1').innerHTML = obj.rand1;document.getElementById('rand2').innerHTML = obj.rand2; console.log(obj.rand1);console.log(obj.rand2); } window.onload = function(event) { init(); }</script></html>";
+
+// The JSON library uses static memory, so this will need to be allocated:
+StaticJsonDocument<200> doc_tx;                       // provision memory for about 200 characters
+StaticJsonDocument<200> doc_rx;
+StaticJsonDocument<200> doc_tx1;
+
+//List of DLSims
+IPAddress ipAddr;
+
+
+// We want to periodically send values to the clients, so we need to define an "interval" and remember the last time we sent data to the client (with "previousMillis")
+int interval = 1000;                                  // send data to the client every 1000ms -> 1s
+unsigned long previousMillis = 0;                     // we use the "millis()" command for time reference and this will output an unsigned long
+
+// Initialization of webserver and websocket
+WebServer server(80);                                 // the server uses port 80 (standard port for websites
+WebSocketsServer webSocket = WebSocketsServer(81);    // the websocket uses port 81 (standard port for websockets
+
+void setup() {
+  Serial.begin(115200);                               // init serial port for debugging
+ 
+  WiFi.begin(ssid, password);                         // start WiFi interface
+  Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));     // print SSID to the serial interface for debugging
+ 
+  while (WiFi.status() != WL_CONNECTED) {             // wait until WiFi is connected
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.print("Connected to network with IP address: ");
+  Serial.println(WiFi.localIP());                     // show IP address that the ESP32 has received from router
+  
+  server.on("/", []() {                               // define here wat the webserver needs to do
+    server.send(200, "text\html", webpage);           //    -> it needs to send out the HTML string "webpage" to the client
+    // NOTE: if you use Edge or IE, then use:
+    //server.send(200, "text/html", webpage);
+  });
+  server.begin();                                     // start server
+  
+  webSocket.begin();                                  // start websocket
+  webSocket.onEvent(webSocketEvent);                  // define a callback function -> what does the ESP32 need to do when an event from the websocket is received? -> run function "webSocketEvent()"
+
+  
+  //allows serving of files from SPIFFS
+  if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    }
+    
+
+  
+}
+
+
+void loop() {
+  server.handleClient();                              // Needed for the webserver to handle all clients
+  webSocket.loop();                                   // Update function for the webSockets 
+  
+  unsigned long now = millis();                       // read out the current "time" ("millis()" gives the time in ms since the Arduino started)
+  if ((unsigned long)(now - previousMillis) > interval) { // check if "interval" ms has passed since last time the clients were updated
+    
+    String jsonString = "";                           // create a JSON string for sending data to the client
+    JsonObject object = doc_tx.to<JsonObject>();      // create a JSON Object
+    object["rand1"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
+    object["rand2"] = random(100);
+    serializeJson(doc_tx, jsonString);                // convert JSON object to string
+   // Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
+   // webSocket.broadcastTXT(jsonString);               // send JSON string to clients
+    
+    previousMillis = now;                             // reset previousMillis
+  }
+  //int num_clients=webSocket.connectedClients(true);
+  
+  //Serial.print("Connected Clients.. "+ num_clients);
+ // delay(10);
+ // Serial.println(" out of " + String(WEBSOCKETS_SERVER_CLIENT_MAX));
+ // delay(10);
+  
+}
+
+void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
+  switch (type) {                                     // switch on the type of information sent
+    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
+    //  Serial.println("Client " + String(num) + " disconnected");
+      break;
+    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
+      Serial.println("Client " + String(num) + " connected");
+      Serial.println("Got from Client: " + String((char *)payload));
+
+      char * _payload;
+      
+      _payload=(char *)payload;
+
+      // if client invoked connect api with /?tsn=9090
+
+      if((String(_payload).compareTo("/?tsn=9090"))==0){
+        String _send_spi_response=read_json_file(SPIFFS,"/api_connect_reponse.json");
+        //serializeJson(doc_tx,_send_spi_response);
+        // send the api response to client
+        webSocket.sendTXT(num,_send_spi_response);
+       // Serial.println("Read from file... "+_send_spi_response);
+        
+      }
+      //ipAddr = webSocket.remoteIP(num);
+      //Serial.println("Clients connected " + ipAddr);
+      // optionally you can add code here what to do when connected
+           
+      break;
+    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
+      // try to decipher the JSON string received
+      DeserializationError error = deserializeJson(doc_rx, payload);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+      else {
+        //Serial.println("Got from Client: " + String((char *)payload));
+        char * command=(char*)payload;
+        Serial.println("Got from Client: " + String(command));
+        
+        // JSON string was received correctly, so information can be retrieved:
+        //const char* g_brand = doc_rx["brand"];
+        //const char* g_type = doc_rx["type"];
+        //const int g_year = doc_rx["year"];
+        //const char* g_color = doc_rx["color"];
+        //Serial.println("Received guitar info from user: " + String(num));
+        //Serial.println("Brand: " + String(g_brand));
+        //Serial.println("Type: " + String(g_type));
+        //Serial.println("Year: " + String(g_year));
+        //Serial.println("Color: " + String(g_color));
+        
+        //received data from client(num)
+
+        //String data_key_from_client = doc_rx["data_key"];
+        //String data_val_from_client = doc_rx["data_val"];
+       // Serial.println(data_key_from_client);
+       // Serial.print(data_val_from_client);
+       // if (data_val_from_client.compareTo("PING")==0){
+        //       Serial.println("Client " + String(num) + " is alive");
+       // }
+          
+       // String jsonString1 = "";                           // create a JSON string for sending data to the client
+       // JsonObject object1 = doc_tx1.to<JsonObject>();      // create a JSON Object
+      //  object1["rand1"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
+       // object1["rand2"] = random(100);
+      //  serializeJson(doc_tx1, jsonString1);                // convert JSON object to string
+        //Serial.println(jsonString1);                       // print JSON string to console for debug purposes (you can comment this out)
+        //send message to connected client`
+      //  webSocket.sendPing(num,jsonString1);
+      }
+      Serial.println("");
+      break;
+  }
+}
+
+String read_json_file(fs::FS &fs, const char * path)
+{
+     
+   Serial.printf("Reading file... ");
+
+   String _json_data="";
+   String line_buff="";
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return "Error";
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+     // Serial.write(file.read());
+     //file.read(line_buff,1024);
+     //file.readBytesUntil('\n', line_buff, 1024);
+      //char* str=(char*)line_buff;
+     // String str=String(line_buff);
+
+     line_buff=file.readStringUntil('\n');
+     _json_data.concat(line_buff);
+     
+     //Serial.println(line_buff);
+    }
+    file.close();
+    //Serial.println("_json_data "+_json_data);
+    return (String)_json_data;
+}
